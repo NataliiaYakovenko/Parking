586409b1874699a18a58740852aabc8f9c1d7cc7
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-12-01 17:02:42 +0200

    Added protocol migrations

---
+++ server/migrations/2-create-protocols.js
@@ -0,0 +1,50 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("protocols", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      serviceNotes: {
+        allowNull: false,
+        field: "service_notes",
+        type: Sequelize.STRING,
+      },
+      officerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: "officer_id",
+        references: {
+          model: {
+            tableName: "park_officers",
+            key: "id",
+          },
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      fineAmount: {
+        allowNull: false,
+        type: Sequelize.DOUBLE,
+        field: "fine_amount",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: "created_at",
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: "updated_at",
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("protocols");
+  },
+};
