bf4e51e4132e2d928f9a44e77d331a6c0bc79da7
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-12-01 18:08:25 +0200

    Corrected migrations

--- server/migrations/1-create-park-officers.js
+++ server/migrations/1-create-park-officers.js
@@ -1,7 +1,7 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  async up(queryInterface, Sequelize){
     await queryInterface.createTable("park_officers", {
       id: {
         allowNull: false,
@@ -35,7 +35,7 @@ module.exports = {
       },
     });
   },
-  down: async (queryInterface, Sequelize) => {
+  async down(queryInterface, Sequelize){
     await queryInterface.dropTable("park_officers");
   },
 };
--- server/migrations/3-create-images.js
+++ server/migrations/3-create-images.js
@@ -1,8 +1,8 @@
 "use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface("images", {
+  async up(queryInterface, Sequelize){
+    await queryInterface.createTable("images", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -38,7 +38,7 @@ module.exports = {
       },
     });
   },
-  down: async (queryInterface, Sequelize) => {
+  async down(queryInterface, Sequelize){
     await queryInterface.dropTable("images");
   },
 };
