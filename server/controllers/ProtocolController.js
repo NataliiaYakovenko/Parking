0fbdab10d25feaa0a12c89fa54c382dfe2a30487
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-12-29 22:52:10 +0200

    Created protocol controller

---
+++ server/controllers/ProtocolController.js
@@ -0,0 +1,176 @@
+const createHttpError = require("http-errors");
+const { Protocol, ParkOfficer, Image } = require("../models");
+const { where } = require("sequelize");
+
+module.exports.getAllProtocols = async (req, res, next) => {
+  try {
+    const { pagination } = req;
+
+    const protocols = await Protocol.findAll({
+      include: [
+        {
+          model: "ParkOfficer",
+          attributes: ["id", "full_name", "badge_name"],
+          as: "parkOfficer",
+        },
+        {
+          model: "Image",
+          attributes: ["id", "path"],
+          as: "image",
+        },
+      ],
+      order: [["updated_at", "DESC"]],
+      ...pagination,
+    });
+
+    if (!pagination.length) {
+      return next(createHttpError(404, "Protocols not found"));
+    }
+
+    return res.status(200).send({ data: protocols });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getAllProtocolsByOfficerId = async (req, res, next) => {
+  try {
+    const {
+      params: { officerId },
+    } = req;
+
+    const protocols = await Protocol.findAll({
+      where: { officerId },
+      include: [
+        {
+          model: "ParkOfficer",
+          attributes: ["id", "full_name", "badge_name"],
+          as: "parkOfficer",
+        },
+        {
+          model: "Image",
+          attributes: ["id", "path"],
+          as: "image",
+        },
+      ],
+      order: [["updated_at", "DESC"]],
+    });
+
+    if (!pagination.length) {
+      return next(createHttpError(404, "Protocols not found"));
+    }
+
+    return res.status(200).send({ data: protocols });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.createProtocol = async (req, res, next) => {
+  try {
+    const { body, files } = req;
+    const createdProtocol = await Protocol.created(body);
+
+    if (!createdProtocol) {
+      return next(createHttpError(400, "Protocol not created"));
+    }
+
+    if (files?.length) {
+      const images = files.map((file) => ({
+        path: file.filename,
+        protocolId: createdProtocol.id,
+      }));
+
+      await Image.bulkCreate(images);
+    }
+    const protocolWithData = await Protocol.findAll({
+      where: { id: createdProtocol.id },
+      include: [
+        {
+          model: "ParkOfficer",
+          attributes: ["id", "full_name", "badge_name"],
+          as: "parkOfficer",
+        },
+        {
+          model: "Image",
+          attributes: ["id", "path"],
+          as: "image",
+        },
+      ],
+      order: [["updated_at", "DESC"]],
+    });
+
+    return res.status(201).send({ data: protocolWithData });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateProtocolById = async (req, res, next) => {
+  try {
+    const {
+      params: { id },
+      body,
+      files,
+    } = req;
+
+    const [count, [updatedProtocol]] = await Protocol.update(body, {
+      where: { id },
+      returning: true,
+    });
+
+    if (files?.length) {
+      const images = files.map((file) => ({
+        path: file.filename,
+        protocolId: updatedProtocol.id,
+      }));
+
+      await Image.bulkCreate(images);
+    }
+
+    if (count === 0) {
+      return next(createHttpError(404), "Protocol not found");
+    }
+
+    const protocolWithData = await Protocol.findAll({
+      where: { id: updatedProtocol.id },
+      include: [
+        {
+          model: "ParkOfficer",
+          attributes: ["id", "full_name", "badge_name"],
+          as: "parkOfficer",
+        },
+        {
+          model: "Image",
+          attributes: ["id", "path"],
+          as: "image",
+        },
+      ],
+      order: [["updated_at", "DESC"]],
+    });
+
+    return res.status(200).send({ data: protocolWithData });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteProtocolById = async (req, res, next) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+
+    const count = await Protocol.destroy({
+      where: { id },
+    });
+
+    if (count === 0) {
+      return next(createHttpError(404), "Protocol not found");
+    }
+
+    return res.status(200);
+  } catch (error) {
+    next(error);
+  }
+};
