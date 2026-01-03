c54ee16b6d4ec51bb1b359a2984036e7a6c014ec
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-01-03 22:01:12 +0200

    Created park officer controller

---
+++ server/controllers/ParkOfficersController.js
@@ -0,0 +1,85 @@
+const { ParkOfficer } = require("../models");
+const createHttpError = require("http-errors");
+
+module.exports.getAllParkOfficers = async (req, res, next) => {
+  try {
+    const parkOfficers = await ParkOfficer.findAll();
+
+    return res.status(200).send({ data: parkOfficers });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getParkOfficerById = async (req, res, next) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+
+    const parkOfficer = await ParkOfficer.findAll({
+      where: { id },
+    });
+
+    if (!parkOfficer) {
+      return next(createHttpError(404), "Park offecer not found");
+    }
+    return res.status(200).send({ data: parkOfficer });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.createParkOfficer = async (req, res, next) => {
+  try {
+    const { body } = req;
+
+    const createdParkOfficer = await ParkOfficer.create(body);
+
+    if (!createdParkOfficer) {
+      return next(createHttpError(400, "Park officer not created"));
+    }
+    return res.status(201).send({ data: createdParkOfficer });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updatrParkOfficerById = async (req, res, next) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const { body } = req;
+
+    const [count, [updatedParkOfficer]] = await ParkOfficer.update(body, {
+      where: { id },
+      returning: true,
+    });
+
+    if (count === 0) {
+      return next(createHttpError(404), "Park offecer not found");
+    }
+
+    return res.status(200).send({ data: updatedParkOfficer });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteParkOfficerById = async (req, res, next) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+
+    const count = await ParkOfficer.destroy({ where: { id } });
+    if (count === 0) {
+      return next(createHttpError(404), "Park offecer not found");
+    }
+
+    return res.status(200);
+  } catch (error) {
+    next(error);
+  }
+};
