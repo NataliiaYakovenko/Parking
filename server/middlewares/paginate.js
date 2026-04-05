2682bec6d8d49795fb6d9704faf671a3ee385401
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-04-05 16:41:26 +0300

    Add paginate midleware

---
+++ server/middlewares/paginate.js
@@ -0,0 +1,22 @@
+const MAX_LIMIT = 5;
+
+module.exports = async (req, res, next) => {
+  try {
+    const { limit, offset } = req.query;
+  
+    if (!limit && !offset) {
+      req.pagination = {
+        limit: 5,
+        offset: 0,
+      };
+    } else {
+      req.pagination = {
+        limit: limit > MAX_LIMIT || limit <= 0 ? MAX_LIMIT : limit,
+        offset: offset < 0 ? 0 : offset,
+      };
+    }
+    next()
+  } catch (error) {
+    next(error);
+  }
+};
