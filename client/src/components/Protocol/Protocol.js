66e40e8c6446e0b55ad3a87b6a18555f3cf1d7bd
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-07-12 20:15:22 +0300

    Added sluder to protocol

--- client/src/components/Protocol/Protocol.js
+++ client/src/components/Protocol/Protocol.js
@@ -1,7 +1,17 @@
 import React from "react";
-import styles from './Protocol.module.scss';
+import styles from "./Protocol.module.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Protocol = ({ protocol }) => {
+  const setting = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
     <article className={styles.cardWrapper}>
@@ -17,13 +27,18 @@ const Protocol = ({ protocol }) => {
       <p>Officer full name:{protocol.parkOfficer.full_name}</p>
       <p>Officer badge number: {protocol.parkOfficer.badge_number}</p>
 
-      {protocol.image.map((currentImage) => (
-        <img
-          key={currentImage.id}
-          src={`http://localhost:5000/images/${currentImage.path}`}
-          alt={protocol.id}
-        />
-      ))}
+      {protocol.image.length > 0 && (
+        <Slider {...setting} className={styles.slider}>
+          {protocol.image.map((currentImage) => (
+            <img
+              className={styles.img}
+              key={currentImage.id}
+              src={`http://localhost:5000/images/${currentImage.path}`}
+              alt={protocol.id}
+            />
+          ))}
+        </Slider>
+      )}
     </article>
   );
 };
