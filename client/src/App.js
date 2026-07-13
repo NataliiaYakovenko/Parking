d6c899e5edc89ece8184333b49a2f934e1e98add
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-07-13 20:52:39 +0300

    Added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
-// import ParkOfficersPage from "./pages/parkOfficersPage/parkOfficersPage";
+import ParkOfficersPage from "./pages/parkOfficersPage/parkOfficersPage";
 import ProtocolsPage from "./pages/protocolsPage/protocolsPage";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import styles from "./App.module.scss";
 
 function App() {
-  return <ProtocolsPage />;
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul className={styles.navList}>
+          <li>
+            <Link className={styles.link} to="/">
+              Officers
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} to="/protocols">
+              Protocols
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<ParkOfficersPage />} />
+        <Route path="/protocols" element={<ProtocolsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
